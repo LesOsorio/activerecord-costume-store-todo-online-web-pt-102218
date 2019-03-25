9bf1139a82f68dd25a71cb8a10c8025a0079bae8
Leslie Osorio <lesosorio17@gmail.com>
2019-03-25 16:52:13 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,13 +1,13 @@
 class CostumeStore <ActiveRecord::Migration[4.2]
-  def change 
+  def change
     create_table :costume_stores do |t|
       t.string :name
-      t.string :location 
-      t.integer :costume_inventory 
-      t.integer :employees_count 
-      t.boolean :true 
-      t.integer :opening_time 
-      t.integer :closing_time 
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees_count
+      t.boolean :true
+      t.integer :opening_time
+      t.integer :closing_time
     end 
   end
-end 
+end
